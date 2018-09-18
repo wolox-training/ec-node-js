5b26ecc759d89585526ee37f96c6311867543f69
Emanuel Casco <cascoemanuel@gmail.com>
2018-09-18 12:35:33 -0300

    Refactor in validations middleware

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,16 @@
 const usersController = require('./controllers/users'),
   albumsController = require('./controllers/albums'),
-  validations = require('./middlewares/validations'),
+  validate = require('./middlewares/validations'),
   auth = require('./middlewares/auth');
 
 exports.init = app => {
-  app.get('/users', [auth.authenticate, validations.paginateValidator], usersController.getUsers);
-  app.post('/users', [validations.userValidator], usersController.create);
-  app.post('/users/sessions', [validations.signinValidator], usersController.signin);
+  app.get('/users', [auth.authenticate, validate.pagination], usersController.getUsers);
+  app.post('/users', [validate.createUser], usersController.create);
+  app.post('/users/sessions', [validate.signin], usersController.signin);
   app.post('/users/invalidate_all', [auth.authenticate, auth.isAdmin], usersController.invalidateAllTokens);
   app.post(
     '/admin/users',
-    [auth.authenticate, auth.isAdmin, validations.userValidator],
+    [auth.authenticate, auth.isAdmin, validate.createAdmin],
     usersController.createAdmin
   );
   app.get('/albums', [auth.authenticate], albumsController.getAll);
