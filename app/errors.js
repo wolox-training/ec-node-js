172a2bc35c90e84d4f2e28f5e8de6572bd09e039
Emanuel Casco <cascoemanuel@gmail.com>
2018-09-27 11:30:29 -0300

    Implement new errors

--- app/errors.js
+++ app/errors.js
@@ -3,11 +3,17 @@ const internalError = (message, internalCode) => ({
   internalCode
 });
 
+exports.EXTERNAL_API_ERROR = 'external_api_error';
+exports.externalAPIError = () => internalError('Error consulting external API', exports.EXTERNAL_API_ERROR);
+
 exports.DEFAULT_ERROR = 'default_error';
 exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
 
 exports.REQUEST_ERROR = 'request_error';
 exports.requestError = message => internalError(message, exports.REQUEST_ERROR);
+
+exports.NOT_FOUND = 'not_found';
+exports.notFound = message => internalError(message, exports.NOT_FOUND);
 
 exports.DATABASE_ERROR = 'database_error';
 exports.databaseError = message => internalError(message, exports.DATABASE_ERROR);
