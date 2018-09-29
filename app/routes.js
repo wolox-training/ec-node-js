ff7e30171a7e39d9a79b65bfc6356bebe9126242
Emanuel Casco <cascoemanuel@gmail.com>
2018-09-29 20:59:16 -0300

    Add routes

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,26 @@
 const usersController = require('./controllers/users'),
   albumsController = require('./controllers/albums'),
   validate = require('./middlewares/validations'),
+  accessControl = require('./middlewares/accessControl'),
   auth = require('./middlewares/auth');
 
 exports.init = app => {
   app.get('/users', [auth.authenticate, validate.pagination], usersController.getUsers);
   app.post('/users', [validate.createUser], usersController.create);
   app.post('/users/sessions', [validate.signin], usersController.signin);
-  app.post('/users/invalidate_all', [auth.authenticate, auth.isAdmin], usersController.invalidateAllTokens);
+  app.post(
+    '/users/invalidate_all',
+    [auth.authenticate, accessControl.requireAccessLevel('admin')],
+    usersController.invalidateAllTokens
+  );
+  app.get(
+    '/users/:userId/albums',
+    [auth.authenticate, validate.params, accessControl.requireAccessLevel('ownerOrAdmin')],
+    usersController.listAlbums
+  );
   app.post(
     '/admin/users',
-    [auth.authenticate, auth.isAdmin, validate.createAdmin],
+    [auth.authenticate, accessControl.requireAccessLevel('admin'), validate.createAdmin],
     usersController.createAdmin
   );
   app.get('/albums', [auth.authenticate], albumsController.fetchAll);
